07466b7f62993203485ca806d6d6b78e05365b1f
Corey Oordt <coordt@washingtontimes.com>
2009-11-10 07:53:58 -0500

    Renamed a few things

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(name='cookiesession',
+      version='0.1',
+      description='A secure way to use cookies for sessions',
+      long_description='',
+      author='Justin Quick, The Washington Times',
+      author_email='jquick@washingtontimes.com',
+      url='http://opensource.washingtontimes.com/projects/cookiesession/',
+      packages=['cookiesession'],
+      classifiers=['Development Status :: 4 - Beta',
+          'Framework :: Django',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License',
+          ],
+      )
