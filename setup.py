10b859c26c562d5635ef3d713df4b889c9f600fd
Corey Oordt <coordt@washingtontimes.com>
2009-11-10 09:33:20 -0500

    Updated the setup.py file and maniest template

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
+#!/usr/bin/env python
+
 from distutils.core import setup
+try:
+    f = open('README.rst', 'r')
+    long_description = f.read()
+    f.close()
+except IOError:
+    long_description = ''
 
 setup(name='cookiesession',
       version='0.1',
-      description='A secure way to use cookies for sessions',
-      long_description='',
+      description='A secure way to hold Django session data in cookies',
+      long_description=long_description,
       author='Justin Quick, The Washington Times',
       author_email='jquick@washingtontimes.com',
       url='http://opensource.washingtontimes.com/projects/cookiesession/',
