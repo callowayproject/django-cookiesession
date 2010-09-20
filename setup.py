b4fe49bb073da4657d8f0b0d5eed5eae6088c0aa
Justin Quick <justquick@gmail.com>
2010-09-20 11:31:54 -0400

    versionbump, setuptools, new url

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
-#!/usr/bin/env python
+from setuptools import setup, find_packages
 
-from distutils.core import setup
 try:
-    f = open('README.rst', 'r')
-    long_description = f.read()
-    f.close()
+    long_description = open('README.rst').read()
 except IOError:
     long_description = ''
 
-setup(name='cookiesession',
-      version='0.1',
+packages = find_packages()
+packages.remove('example')
+
+setup(name='django-cookiesession',
+      version='0.1.1',
       description='A secure way to hold Django session data in cookies',
       long_description=long_description,
       author='Justin Quick, The Washington Times',
       author_email='jquick@washingtontimes.com',
-      url='http://opensource.washingtontimes.com/projects/cookiesession/',
-      packages=['cookiesession'],
+      url='http://github.com/washingtontimes/django-cookiesession',
+      packages=packages,
       classifiers=['Development Status :: 4 - Beta',
           'Framework :: Django',
           'Intended Audience :: Developers',
